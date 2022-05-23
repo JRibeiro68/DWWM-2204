0bab3c405461d8ca5635855bf60aba19ee070ee7
JRibeiro68 <joseribeiro68@hotmail.fr>
2022-05-23 16:22:25 +0200

    mise a jour exercice algo

--- JR_algo/Exercice 2.3/Exercice 2.3/Program.cs
+++ JR_algo/Exercice 2.3/Exercice 2.3/Program.cs
@@ -8,20 +8,25 @@ age = int.Parse(Console.ReadLine());
 diffPlus = age - 60;
 diffMoins = 60 - age;
 
-if (age > 60)
+if (age < 0 )
 {
-    Console.WriteLine("Vous êtes à la retraite depuis " + diffPlus + " années");
-
-     else
-        if (age < 60)
+    Console.WriteLine("L'âge saisi n'est pas valide");
+}
+else
+{
+    if (age < 60)
     {
-        Console.WriteLine.("Il vous reste " + diffMoins + " années avant la retraite");
+        Console.WriteLine("il vous reste " + diffMoins);
+    }
+    else
+    {
+        if (age > 60)
+        {
+            Console.WriteLine("Vous ètes à la retraite " + diffPlus + " années");
+        }
+        else
+        {
+            Console.WriteLine("C'est le moment de prendre votre retraite");
+        }
     }
 }
-else
-    if (age == 60)
-{
-    Console.WriteLine("C'est le moment de prendre sa retraite");
-}
-else
-    Console.WriteLine("La valeur fournie n'est pas un âge légale");
