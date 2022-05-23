6f149f5186531e2a52171538ad165d2bfb115a36
JRibeiro68 <joseribeiro68@hotmail.fr>
2022-05-23 16:56:35 +0200

    update exercice algo

---
+++ JR_algo/Exercice2.4 Tri des nombres++/Exercice2.4 Tri des nombres++/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int nbrA, nbrB, nbrC
+
+Console.WriteLine("entrez un premier nombre entier");
+nbrA = int.Parse(Console.ReadLine());
+Console.WriteLine("entrez un deuxième nombre premier");
+nbrB = int.Parse(Console.ReadLine());
+Console.WriteLine("entrez un troisième nombre premier");
+nbrC = int.Parse(Console.ReadLine());
+
+if (nbrA < nbrB) &(nbrA < nbrC) & (nbrB < nbrC);
+{
+    Console.WriteLine(+ nbrC+ ">" + nbrB + ">" + nbrA);
+}
