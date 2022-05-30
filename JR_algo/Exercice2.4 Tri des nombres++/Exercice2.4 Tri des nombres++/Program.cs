1ef7a752030fb45dc5f9eb32df6af97c47d7f3ca
JRibeiro68 <joseribeiro68@hotmail.fr>
2022-05-30 15:32:06 +0200

    exercie algo

--- JR_algo/Exercice2.4 Tri des nombres++/Exercice2.4 Tri des nombres++/Program.cs
+++ JR_algo/Exercice2.4 Tri des nombres++/Exercice2.4 Tri des nombres++/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int nbrA, nbrB, nbrC
+int nbrA, nbrB, nbrC;
 
 Console.WriteLine("entrez un premier nombre entier");
 nbrA = int.Parse(Console.ReadLine());
@@ -9,7 +9,27 @@ nbrB = int.Parse(Console.ReadLine());
 Console.WriteLine("entrez un troisième nombre premier");
 nbrC = int.Parse(Console.ReadLine());
 
-if (nbrA < nbrB) &(nbrA < nbrC) & (nbrB < nbrC);
+if ((nbrA > nbrB) && (nbrB > nbrC))
 {
-    Console.WriteLine(+ nbrC+ ">" + nbrB + ">" + nbrA);
+    Console.WriteLine(+nbrA + ">" + nbrB + ">" + nbrC);
+}
+else if ((nbrA > nbrC) && (nbrC > nbrB))
+{
+    Console.WriteLine(+nbrA + ">" + nbrC + ">" + nbrB);
+}
+if ((nbrB > nbrA) && (nbrA > nbrC))
+{
+    Console.WriteLine(nbrB + ">" + nbrA + ">" + nbrC); 
+}
+if ((nbrB > nbrC) && (nbrC > nbrA))
+{
+    Console.WriteLine(nbrB + ">" + nbrC + ">" + nbrA);
+}
+if ((nbrC > nbrB) && (nbrB > nbrA))
+{
+    Console.WriteLine(nbrC + ">" + nbrB + ">" + nbrA);
+}
+if ((nbrC > nbrA) && (nbrA > nbrB))
+{
+    Console.WriteLine(nbrC + ">" + nbrA + ">" + nbrB);
 }
