18feecd80762382d4dcb72d4a8108c1e75004e0c
JRibeiro68 <joseribeiro68@hotmail.fr>
2022-05-31 15:49:40 +0200

    new exo c#

--- JR_algo/Exercice 2.5 la bicyclette/Exercice 2.5 la bicyclette/Program.cs
+++ JR_algo/Exercice 2.5 la bicyclette/Exercice 2.5 la bicyclette/Program.cs
@@ -1,2 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+bool Beau, BonEtat, ReparationImmediate, LivreDispoSalon, LivreDispoBibliotheque;
+
+Console.WriteLine("Fait - il beau ce matin?");
+Beau = bool.Parse(Console.ReadLine());
+
+
+
+if (Beau == true)
+{
+    Console.WriteLine("La bicyclette est-elle en bon état?");
+    BonEtat = bool.Parse(Console.ReadLine());
+    if (BonEtat == false)
+    {
+        Console.WriteLine("Allez chez le garagiste");
+        Console.WriteLine("La réparation est - elle immédiate ?");
+        ReparationImmediate = bool.Parse(Console.ReadLine());
+        if (ReparationImmediate == false)
+        {
+            Console.WriteLine("Allez à pied à l'étang et ceuillir des joncs");
+        }
+    }
+    else
+        Console.WriteLine("Allez fair eune ballade à bicyclette");
+}
+else
+{
+    Console.WriteLine("Consacrer ma journée à la lecture");
+}
+
+{
+    Console.WriteLine("Le livre Game of thrones est - il dans le salon ?");
+    LivreDispoSalon = bool.Parse(Console.ReadLine());
+    if (LivreDispoSalon == false)
+    {
+        Console.WriteLine("Allez à la bibliothèque municipale");
+        Console.WriteLine("Le livre est-il disponible à la bibliothèque?");
+    }
+    LivreDispoBibliotheque = bool.Parse(Console.ReadLine());
+    if (LivreDispoBibliotheque == false)
+    {
+        Console.WriteLine("Emprunter un roman policier.");
+    }
+    Console.WriteLine("Rentrer directrement à la maison");
+
+
+    {
+        Console.WriteLine("M'installer corectement dans un fauteuil");
+    }
+}
---
+++ JR_algo/Exercice 3.1 Controler la saisie/Exercice 3.1 Controler la saisie/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string prenom;
+
+do
+{
+    Console.WriteLine("Entrez votre prénom");
+    prenom = (Console.ReadLine());
+}
+while (prenom.Length <= 2);
+Console.WriteLine("Bonjour " + prenom);
