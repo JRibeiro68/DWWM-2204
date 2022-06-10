fc63263bcdef88c972b3dfb1bb20dbc96b8dcd26
José <toto@crm.fr>
2022-06-10 11:11:23 +0200

    mise a jour exercice

--- JR_algo/Exercice 3.5 Nombre premier/Exercice 3.5 Nombre premier/Program.cs
+++ JR_algo/Exercice 3.5 Nombre premier/Exercice 3.5 Nombre premier/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿float nbrPremier;
+string resultat = "est un nombre premier";
+
+
+Console.WriteLine("Entrer un nombre premier");
+nbrPremier = float.Parse(Console.ReadLine());
+
+
+for (int i = 2; i < nbrPremier; i++)
+
+    if (nbrPremier % i == 0) 
+    {
+        resultat = nbrPremier + " n'est pas un nombre premier";
+    }
+Console.WriteLine(resultat);
+
+
+
