530163a5ea042cb426bbe3a8d5636e8dc3fa2d09
José <toto@crm.fr>
2022-06-17 08:16:33 +0200

    mise à jour

---
+++ JR_algo/Exercice 3.7 Barnabé fait ses courses/Exercice 3.7 Barnabé fait ses courses/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string strLigne;
+int entier;
+int reste;
+
+
+Console.WriteLine("Saisissez un entire :");
+strLigne = Console.ReadLine();
+
+entier = Int32.Parse(strLigne);
+
+reste = entier % 2;
+if (reste == 0)
+{
+    Console.WriteLine("Entier pair !");
+}
+
